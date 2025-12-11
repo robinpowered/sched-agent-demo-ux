6081effb6f48f23083bae2507894403d861406ba
javasteve99 <steve.hickey@robinpowered.com>
2025-12-11 13:01:33 -0500

    Fix navigation problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './styles/globals.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>,
+// Get the base URL from Vite config (handles GitHub Pages subdirectory)
+const baseUrl = import.meta.env.BASE_URL;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename={baseUrl}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
